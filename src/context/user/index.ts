d43e90bcdc0be057e0336538c24845b272b3ccc3
tobias-fernandes <tobys312@gmail.com>
2025-11-16 02:25:06 -0300

    feat: set user data to zustand

--- src/context/user/index.ts
+++ src/context/user/index.ts
@@ -1,10 +1,15 @@
 import { create } from "zustand";
 import { IStoreUser } from "./types";
+import { getUserName } from "@/server/user/name/get-name/actions";
+import { getUserAvatar } from "@/server/user/avatar/get-avatar/actions";
+import { getUserEmail } from "@/server/user/email/get-email/actions";
 
 const useStoreUser = create<IStoreUser>((set) => ({
   state: {
     name: "",
     avatarUrl: "",
+    email: "",
+    isLoading: true,
   },
 
   actions: {
@@ -22,6 +27,24 @@ const useStoreUser = create<IStoreUser>((set) => ({
         state: {
           ...state.state,
           avatarUrl: url,
+        },
+      }));
+    },
+
+    fetchUserData: async () => {
+      const [name, avatarUrl, email] = await Promise.all([
+        getUserName(),
+        getUserAvatar(),
+        getUserEmail(),
+      ]);
+
+      set((state) => ({
+        state: {
+          ...state.state,
+          name: name ?? state.state.name ?? "unknown user",
+          avatarUrl: avatarUrl ?? state.state.avatarUrl,
+          email: email ?? state.state.email,
+          isLoading: false,
         },
       }));
     },
--- src/context/user/types.ts
+++ src/context/user/types.ts
@@ -2,11 +2,13 @@ interface IStoreUser {
   state: {
     name: string;
     avatarUrl: string;
+    email: string;
+    isLoading: boolean;
   };
   actions: {
     setName: (name: string) => void;
     setAvatarUrl: (url: string) => void;
+    fetchUserData: () => Promise<void>;
   };
 }
-
 export type { IStoreUser };
