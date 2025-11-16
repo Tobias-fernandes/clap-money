2e965cc4ce53a7962ac2fa66b874e9783d352ed2
tobias-fernandes <tobys312@gmail.com>
2025-11-16 00:19:43 -0300

    fix: change button text color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-clap-green text-secondary hover:bg-clap-green/90",
+          "bg-clap-green text-secondary dark:text-primary hover:bg-clap-green/90",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
